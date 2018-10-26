f7cbfc2059d478689ad815b7428355e80d61b270
Owais Imam <44465478+OwaisImam@users.noreply.github.com>
2018-10-26 18:43:09 +0500

    Add files via upload

---
+++ Permutation/src/permute.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Syed
+ */
+class permute {
+    
+     public String permut(String str, int l, int r)
+	{
+		if (l == r)
+			System.out.println(str);
+		else
+		{
+			for (int i = l; i <= r; i++)
+			{
+				str = swap(str,l,i);
+				permut(str, l+1, r);
+				str = swap(str,l,i);
+			}
+		}
+	return str;
+        }
+  
+   	public String swap(String a, int i, int j)
+	{
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i] ;
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}
+    
+}
